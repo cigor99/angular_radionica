e89f8d25995230b4c847bafa404275bf680f4982
Igor Lukic <igor.lukic@marbleit.rs>
2023-11-15 19:04:10 +0100

    Add empty components model and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersListComponent } from './components/users/users-list/users-list.component';
+import { UsersCreateComponent } from './components/users/users-create/users-create.component';
+import { UsersDetailsComponent } from './components/users/users-details/users-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersListComponent,
+    UsersCreateComponent,
+    UsersDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/users/users-create/users-create.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'users-create',
+  templateUrl: './users-create.component.html',
+  styleUrl: './users-create.component.scss'
+})
+export class UsersCreateComponent {
+
+}
