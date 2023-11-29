520bf8898e286fbe8188886abd4de93dab820947
Igor Lukic <igor.lukic@marbleit.rs>
2023-11-29 17:57:24 +0100

    Add action button

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import {LayoutComponent} from "./components/layout/layout.component";
 import { TableComponent } from './components/table/table.component';
 import {CommonModule} from "@angular/common";
+import { ActionButtonComponent } from './components/action-button/action-button.component';
 
 @NgModule({
   declarations: [
       LayoutComponent,
       TableComponent,
+      ActionButtonComponent,
   ],
     exports: [
         LayoutComponent,
         TableComponent,
+        ActionButtonComponent
     ],
     imports: [
         CommonModule
